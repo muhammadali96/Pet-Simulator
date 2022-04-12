8348c57c22a8753fae0dc9b11eca040d22eb7748
muhammadali96 <ali2912@outlook.com>
2022-04-13 00:20:08 +0100

    Fixed shop bug

--- Assets/Scripts/Shop Scripts/ShopManagerScript.cs
+++ Assets/Scripts/Shop Scripts/ShopManagerScript.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class ShopManagerScript : MonoBehaviour
 {
 
-    public int[,] shopItems = new int[5,5];
+    public int[,] shopItems = new int[6,6];
     public float coins;
     public Text CoinsTXT;
 
@@ -21,18 +21,21 @@ public class ShopManagerScript : MonoBehaviour
         shopItems[1, 2] = 2;
         shopItems[1, 3] = 3;
         shopItems[1, 4] = 4;
+        shopItems[1, 5] = 5;
 
         //Price
         shopItems[2, 1] = 10;
         shopItems[2, 2] = 20;
         shopItems[2, 3] = 30;
         shopItems[2, 4] = 40;
+        shopItems[2, 5] = 50;
 
         //Quantity
         shopItems[3, 1] = 0;
         shopItems[3, 2] = 0;
         shopItems[3, 3] = 0;
         shopItems[3, 4] = 0;
+        shopItems[3, 5] = 0;
 
     }
 
